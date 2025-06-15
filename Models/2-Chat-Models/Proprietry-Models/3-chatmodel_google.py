08cb9b609a3f3a8d92be3a78accd555098147191
ZaraAbrar-Bhatti <zzaranoor8@gmail.com>
2025-06-15 15:23:20 +0500

    Issue with Gemini API key is resolved.

--- Models/2-Chat-Models/Proprietry-Models/3-chatmodel_google.py
+++ Models/2-Chat-Models/Proprietry-Models/3-chatmodel_google.py
@@ -1,9 +1,13 @@
+import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-Google_Chat_Model = ChatGoogleGenerativeAI(model = "Gemini 1.5 Pro")
+Google_Chat_Model = ChatGoogleGenerativeAI(
+    model = "gemini-1.5-flash",
+    google_api_key = os.getenv("GOOGLE_API_KEY")
+    )
 
 response = Google_Chat_Model.invoke("What is meant by search engine?")
 
